578e7d84ac9ca69a8c1365fa433e212a78135656
Will Baxter <bax.will@googlemail.com>
2024-06-18 13:51:04 +0100

    Edit film by ID

--- src/controllers/filmsControllers.js
+++ src/controllers/filmsControllers.js
@@ -1,32 +1,45 @@
 const data = require("../../data/index.js");
 const films = data.films;
-let idCounter = 5
+let idCounter = 5;
 
-function getAllFilms(req, res){
-    res.status(200).json({ films })
+function getAllFilms(req, res) {
+  res.status(200).json({ films });
 }
 
-function createFilm(req, res){
-    const film = req.body
-    film.id = idCounter
-    films.push(film)
-    idCounter++
-    res.status(201).json({ film })
+function createFilm(req, res) {
+  const film = req.body;
+  film.id = idCounter;
+  films.push(film);
+  idCounter++;
+  res.status(201).json({ film });
 }
 
-
-function getFilmById(req, res){
-    const filmId = Number(req.params.id)
-    const film = films.find((film) => film.id === filmId)
-    res.status(200).json({ film })
+function getFilmById(req, res) {
+  const filmId = Number(req.params.id);
+  const film = films.find((film) => film.id === filmId);
+  res.status(200).json({ film });
 }
 
-function deleteFilmById(req, res){
-    const filmId = Number(req.params.id)
-    const film = films.find((film) => film.id === filmId)
-    const index = films.indexOf(film)
-    films.splice(index, 1)
-    res.status(200).json( {film} )
+function deleteFilmById(req, res) {
+  const filmId = Number(req.params.id);
+  const film = films.find((film) => film.id === filmId);
+  const index = films.indexOf(film);
+  films.splice(index, 1);
+  res.status(200).json({ film });
 }
 
-module.exports = { getAllFilms, createFilm, getFilmById, deleteFilmById }
+function updateFilmById(req, res) {
+  const updatedParameters = req.body;
+  const filmId = Number(req.params.id);
+  const film = films.find((film) => film.id === filmId);
+  Object.assign(film, updatedParameters);
+  res.status(200).json({ film });
+}
+
+module.exports = {
+  getAllFilms,
+  createFilm,
+  getFilmById,
+  deleteFilmById,
+  updateFilmById,
+};
--- src/routers/films.js
+++ src/routers/films.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const filmsRouter = express.Router()
-const { getAllFilms, createFilm, getFilmById, deleteFilmById } = require("../controllers/filmsControllers.js")
+const { getAllFilms, createFilm, getFilmById, deleteFilmById, updateFilmById } = require("../controllers/filmsControllers.js")
 
 filmsRouter.get("/", getAllFilms)
 filmsRouter.post("/", createFilm)
 filmsRouter.get("/:id", getFilmById)
 filmsRouter.delete("/:id", deleteFilmById)
+filmsRouter.put("/:id", updateFilmById)
 
 module.exports = filmsRouter
