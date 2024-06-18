db3e65c5dd1ad59ba2f14a49632d9c13d54c03cb
Will Baxter <bax.will@googlemail.com>
2024-06-18 16:48:03 +0100

    Errors thrown for ALL KINDS of book problems

--- src/controllers/booksControllers.js
+++ src/controllers/booksControllers.js
@@ -13,6 +13,12 @@ function getAllBooks(req, res) {
 
 function createBook(req, res) {
     const book = req.body
+    if (!book.title || !book.type || !book.author) {
+        throw new MissingFieldsError('Missing fields in request body')
+    }
+    if (books.find((b) => b.title === book.title)) {
+        throw new DataAlreadyExistsError('A book with the provided title already exists')
+    }
     books.push(book)
     res.status(201).json({ book })
 }
@@ -29,6 +35,9 @@ function getBookById(req, res) {
 function deleteBookById(req, res) {
     const bookId = Number(req.params.id)
     const book = books.find((book) => book.id === bookId)
+    if (!book) {
+        throw new DataNotFoundError('A book the provided ID does not exist')
+    }
     const index = books.indexOf(book)
     books.splice(index, 1)
     res.status(200).json({ book })
@@ -36,8 +45,19 @@ function deleteBookById(req, res) {
 
 function updateBookById(req, res) {
     const updatedParams = req.body
+    if (!updatedParams.title || !updatedParams.type || !updatedParams.author) {
+        throw new MissingFieldsError('Missing fields in request body')
+    }
     const bookId = Number(req.params.id)
     const book = books.find((book) => book.id === bookId)
+    if (!book) {
+        throw new DataNotFoundError('A book the provided ID does not exist')
+    }
+
+    if (books.find((b) => b.title === updatedParams.title)) {
+        throw new DataAlreadyExistsError('A book with the provided title already exists')
+
+    }
     Object.assign(book, updatedParams)
     res.status(200).json({ book })
 }
