c83e3b2867a9eca95b1533eacef28c55a314fff7
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-06-23 15:48:10 +0100

    W2

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,4 +1,15 @@
 // Import data here...
 
+const express = require('express')
+const router = express.Router()
+const { books } = require('../../data')
+
 
 // Write routes here...
+router.get('/', (request, respond) => {
+    respond.status(200).json({ books })
+})
+
+
+
+module.exports = router
